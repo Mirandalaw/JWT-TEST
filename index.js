5856bf048375519c3c312b98b98e1659e74daf7c
JeongPark <74170593+Mirandalaw@users.noreply.github.com>
2023-01-12 21:00:24 +0900

    develop board,user

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
-const userRouter = require('./apis/user/router/userRouter');
-
+const userRouter = require('./apis/user/router/user');
+const boardRouter = require('./apis/board/router/board');
 // app.set("view engine","pug");
 // app.set("views",__dirname+"/views");
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use('/users',userRouter);
+app.use('/board',boardRouter);
+
 
 module.exports = app;
